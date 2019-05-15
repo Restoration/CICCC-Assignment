cf872b8ab47415418ef169d0e9aaf81e88c9a11f
RyotArch <bboy.ryu@icloud.com>
2019-05-14 22:34:37 -0700

    Added fs package

--- Node/filesystem/server.js
+++ Node/filesystem/server.js
@@ -1,9 +1,28 @@
 const Koa = require('koa');
+const fs = require('fs');
+
+
 const app = new Koa();
 
-// response
 app.use(ctx => {
-  ctx.body = 'Hello Koa';
+    ctx.body = 'Hello Koa';
+    //saveFile();
+    //deleteFile();
 });
+
+const saveFile = () => {
+    fs.appendFile('./files/mynewfile1.txt', 'Hello content!', (err) => {
+        if (err) throw err;
+        console.log('Saved!');
+    }); 
+}
+
+const deleteFile = () => {
+    fs.unlink('./files/mynewfile1.txt', (err) => {
+        if (err) throw err;
+        console.log('Delete!');
+    });
+}
+
 
 app.listen(3000);
